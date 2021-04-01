7a016a48cd38363d920782bc751ddaf8b6563c76
Mathieu Brochard <mathieu.brochard@outlook.com>
2021-04-01 16:24:52 +0200

    Check if room is given

--- decifuck/decifuck.py
+++ decifuck/decifuck.py
@@ -19,6 +19,25 @@ def get_deciplus_credentials(secret_name, username):
 
     return list(filter(lambda cred: cred["username"] == username, credentials))[0]
 
+def process_week_slots(session, week_slots, wanted_slots):
+    for wanted_slot in wanted_slots:
+        for day_slots in week_slots:
+            process_day_slots(session, wanted_slot, day_slots)
+
+def process_day_slots(session, wanted_slot, day_slots):
+    wanted_day = wanted_slot["day"].lower()
+    wanted_hour = get_decimal_hour(wanted_slot["hour"])
+    wanted_room = wanted_slot["room"].lower() if "room" in wanted_slot else None
+
+    if day_slots.day == wanted_day:
+        for slot in day_slots.slots:
+            if wanted_room:
+                if slot.hour == wanted_hour and wanted_room == slot.room:
+                    session.book_timeslot(timeslot=slot)
+            else:
+                if slot.hour == wanted_hour:
+                    session.book_timeslot(timeslot=slot)
+
 def handler(event, context):
     secret_name = event["secret_name"]
     username = event["username"]
@@ -29,18 +48,6 @@ def handler(event, context):
 
     week = pendulum.now()
     for _ in range(2):
-        day_slots = session.get_timeslots_for_week(week)
-
-        for wanted_slot in wanted_slots:
-            wanted_day = wanted_slot["day"].lower()
-            wanted_hour = get_decimal_hour(wanted_slot["hour"])
-            wanted_room = wanted_slot["room"].lower()
-
-            for day_slot in day_slots:
-                if day_slot.day == wanted_day:
-                    for slot in day_slot.slots:
-                        if slot.hour == wanted_hour:
-                            session.book_timeslot(timeslot=slot)
-                            return slot.hour
-        
+        week_slots = session.get_timeslots_for_week(week)
+        process_week_slots(session, week_slots, wanted_slots)
         week = week.add(weeks=1)
