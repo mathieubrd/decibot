48283d0c1e96c8c01ad9c22e66529ddab52cac07
Emmanuel Marboeuf <emmanuel@visage.ae>
2021-10-26 01:09:31 +0200

    fixed 2 bugs and added some logs

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,29 @@ import { findCourses } from './helpers'
 export const handler = async (): Promise<void> => {
     const credentials = await getCredentials('decifuck')
 
-    credentials.forEach(async ({email, password}: Credentials) => {
+    for(const {email, password} of credentials) {
         const client = new DeciplusClient(email, password)
         await client.auth()
 
         const wantedSlots = await getWantedSlots(email)
 
         if (wantedSlots.length) {
+            console.info(wantedSlots);
             const fromDate= new Date()
             const toDate = endOfWeek(nextMonday(fromDate))
 
             const courses = await client.getCourses(fromDate, toDate)
-            const wantedSlotsToBook = findCourses(courses, wantedSlots)
+            console.info('courses');
+            console.info(courses);
 
-            await Promise.all(wantedSlotsToBook.map((course: Course): Promise<Course | undefined> => {
+            const wantedSlotsToBook = findCourses(courses, wantedSlots)
+            console.info('wantedSlotsToBook');
+            console.info(wantedSlotsToBook);
+
+            const results = await Promise.all(wantedSlotsToBook.map((course: Course): Promise<Course | undefined> => {
                 return client.bookCourse(course)
-            }))   
+            }))
+            console.info(`successfully booked ${results.length} courses`);
         }
-    })
+    }
 }
